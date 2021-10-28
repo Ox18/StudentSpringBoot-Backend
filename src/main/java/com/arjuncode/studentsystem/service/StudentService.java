a2a45d0367b4636c011ca78a6c8b513e1551b240
Alex <wilmer.delgadoalama@gmail.com>
2021-10-28 18:03:43 -0500

    Add route to get all students

--- src/main/java/com/arjuncode/studentsystem/service/StudentService.java
+++ src/main/java/com/arjuncode/studentsystem/service/StudentService.java
@@ -2,6 +2,10 @@ package com.arjuncode.studentsystem.service;
 
 import com.arjuncode.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+
+    public List<Student> getAllStudents();
 }
